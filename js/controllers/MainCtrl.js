a4007511a6490d9b1fbc979013f232451a87e912
Bowen Liang <simonlbw@gmail.com>
2014-05-20 03:28:35 +0800

    新main页面

--- js/controllers/MainCtrl.js
+++ js/controllers/MainCtrl.js
@@ -1,11 +1,8 @@
 angular.module('MainCtrl', [])
     .controller('MainCtrl', function ($scope, $ionicSideMenuDelegate) {
-        $scope.here = "main";
 
         $scope.toggleLeftSideMenu = function () {
-            alert($ionicSideMenuDelegate.isOpenLeft());
             $ionicSideMenuDelegate.toggleLeft(true ^ $ionicSideMenuDelegate.isOpenLeft());
-
         };
 
     });
